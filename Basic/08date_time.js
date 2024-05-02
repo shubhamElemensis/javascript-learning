e3adeb301dc7d7c3c94fbfecba074692bb07eabe
shubhamElemensis <168633512+shubhamElemensis@users.noreply.github.com>
2024-05-02 05:30:04 +0000

    #

---
+++ Basic/08date_time.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const mydate = new Date(2023,0,21,5,30,59)
+
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.getDate());
+console.log(mydate.getHours());
+console.log(mydate.toTimeString());
+console.log(mydate.toLocaleString());
